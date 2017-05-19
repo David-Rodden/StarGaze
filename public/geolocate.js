9e31fbe4420db83de1d3f6f7422f54daa36277ea
David-Rodden <davidrodden273@gmail.com>
2017-05-19 14:47:04 -0700

    Added lightdata and utilize it through javascript

--- public/geolocate.js
+++ public/geolocate.js
@@ -10,5 +10,22 @@ $(function () {
         myLoc.bindPopup("<b>You are here</b>");
         L.circle([lat, lon], {radius: 200, color: 'white', stroke: false, fillOpacity: 0.5}).addTo(mymap);
     });
+    $.ajax({
+        type: 'GET',
+        url: "https://rodden.000webhostapp.com/lightdata.txt",
+        success: function (data) {
+            var splitdata = data.split("\n");
+            for (var i in splitdata) {
+                var separatedata = splitdata[i].split("\t");
+                if (separatedata.length != 3)    continue;
+                L.circle([separatedata[0], separatedata[1]], {
+                    radius: separatedata[2] * 1000,
+                    color: 'white',
+                    stroke: false,
+                    fillOpacity: 0.5
+                }).addTo(mymap);
+            }
+        }
+    });
     L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZmlyZXZlbmdlMDA3IiwiYSI6ImNqMnA2ZnE1eDAzNzcycm1peHducXhzOXYifQ.-qj_Mswy-MkpXqySSYFQNQ').addTo(mymap);
 });
